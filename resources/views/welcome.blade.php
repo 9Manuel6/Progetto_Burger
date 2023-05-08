7825c1761fbea81edf55e965177e959b66493e4f
9Manuel6 <tedesdchimanuel96@gmial.com>
2023-05-08 20:33:27 +0200

    migliorare il responsive, e aggiungere un po di colore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,30 +5,27 @@
         <h6 class="display-6"> Gnaam Gnammm </h6>
        <div class="container">
         <div class="row">
-            <div class="col-12 ">
-                <img class="imgC img-fluid" src="{{ url('/media/panino.png')}}" alt="panino">
-                <a href=""> 
-                    <button type="button" class="btn btn-warning"> Chiama ora </button>
-                </a>
+            <div class="col-12">
+                <a class="aCustom" href="{{route('burger.index')}}"><img class="imgC img-fluid" src="{{ url('/media/panino.png')}}" alt="panino"></a>
+            </div>
             </div>
         </div>
        </div>
-     
     </header>
     <section class="container-fluid friesBg">
         <div class="row h-50">
-            <div class="col-12 d-flex flex-column justify-content-center align-items-center">
+            <div class="text-center">
                 <h2 class="display-5 p-3"> I preferiti</h2>
                 <h4>Cosa aspetti?? Facci sapere il tuo preferito..!</h4>
             </div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
+            <div class="panino col-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center align-items-center">
                 <a href=""> 
                     <img class="hoverC img-fluid"src="{{ url('/media/panino2.png')}}" alt="">
                 </a>
             </div>
-            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center">
+            <div class="pollo col-6 col-md-6 col-sm-6 d-flex flex-column justify-content-center align-items-center">
                 <a href=""> 
                     <img class="hoverC img-fluid" src="{{url('/media/alette.png')}}" alt="">
                 </a>
